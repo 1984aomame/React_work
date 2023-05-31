f4ae03d53bd94fbb7b90add2d7da334495f4e541
Tomoka Nohara <35630132+1984aomame@users.noreply.github.com>
2023-05-31 06:11:16 +0000

    useEffectの使い方

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,34 @@ import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const { useEffect, useState } = React;
   console.log("さいしょ");
   const [num, setNum] = useState(0);
-  const [faceShowFlag, setFaceShowFlag] = useState(true);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
   const onClickCountUp = () => {
     setNum(num + 1);
   };
   const onClickSwitchShowFlag = () => {
     setFaceShowFlag(!faceShowFlag);
   };
-  //Too many re-renders のエラーになる例
-  if (num % 3 === 0) {
-    setFaceShowFlag(true); //ここでいずれの場合もStateの変更がありAppループになる
-  } else {
-    setFaceShowFlag(false);
-  }
+
+  useEffect(() => {
+    console.log("useeffect");
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true); //関数が毎回呼ばれないように対応
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>JSX記法で書きます</h1>
       <ColorfulMessage color="blue">お元気ですか</ColorfulMessage>
       <ColorfulMessage color="pink">お元気です</ColorfulMessage>
-      <button onClick={onClickCountUp}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
       <br />
       <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
